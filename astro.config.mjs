906e15f79b95e3afa013995dfe5c8135f067e1a0
Barbarach BC <barbarachbcollig@gmail.com>
2025-12-05 00:19:19 +0000

    added party town

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from "@tailwindcss/vite";
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), partytown()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
